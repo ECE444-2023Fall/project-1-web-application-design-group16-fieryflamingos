1790911246e0dfa5ea1d14090cb466f266f9bc6b
Sebastian Czyrny <sebastian.czyrny@hotmail.com>
2023-10-17 11:50:28 -0400

    updated config with whitelisted domains

--- app/config.py
+++ app/config.py
@@ -6,14 +6,12 @@ import secret
 # DEBUG can only be set to True in a development environment for security reasons
 DEBUG = True
 
-# Secret key for generating tokens
-SECRET_KEY = 'houdini'
 
-# Admin credentials
-ADMIN_CREDENTIALS = ('admin', 'pa$$word')
 
 # Database choice
 MONGO_DATABASE_URI = f"mongodb+srv://{secret.userdb}:{secret.passworddb}@cluster0.9dnqbir.mongodb.net/?retryWrites=true&w=majority"
+MONGO_DATABASE = 'test'
+MONGO_DATABASE_ALIAS = 'core'
 
 # Configuration of a Gmail account for sending mails
 MAIL_SERVER = 'smtp.googlemail.com'
@@ -23,6 +21,8 @@ MAIL_USE_SSL = True
 MAIL_USERNAME = 'flask.boilerplate'
 MAIL_PASSWORD = 'flaskboilerplate123'
 ADMINS = ['flask.boilerplate@gmail.com']
+
+DOMAIN_WHITELIST = ["mail.utoronto.ca"]
 
 # Number of times a password is hashed
 BCRYPT_LOG_ROUNDS = 12
