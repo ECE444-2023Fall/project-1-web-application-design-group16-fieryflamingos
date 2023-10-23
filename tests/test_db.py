63fd3f5aa030362cef6085633138943a349e1c64
Sebastian Czyrny <sebastian.czyrny@hotmail.com>
2023-10-23 18:38:23 -0400

    added skeleton test cases for database models

--- tests/test_db.py
+++ tests/test_db.py
@@ -3,21 +3,101 @@ from flask import current_app
 from app import create_app, db
 from app.models import User
 
-class BasicsTestCase(unittest.TestCase):
+import unittest
+from datetime import datetime
+from mongoengine import connect
+from models import User, RegularUser, OrganizationUser, Location, UserInfo, Reply, Comment, Events
+
+connect('test_db', host='localhost', port=27017)
+
+class TestUser(unittest.TestCase):
+
     def setUp(self):
-        self.app = create_app('testing')
-        self.app_context = self.app.app_context()
-        self.app_context.push()
+        self.user = RegularUser(
+            first_name='John',
+            last_name='Doe',
+            email='johndoe@example.com',
+            password='Password@123',
+            preferences=['preference1', 'preference2'],
+            registered_events=[]
+        )
 
-    def tearDown(self):
-        self.app_context.pop()
+    def test_validate_email_validEmail(self):
+        result = self.user.validate_email()
+        self.assertTrue(result)
 
-    def test_user(self):
-        user = User()
-        user.email = "mehak.sharma@mail.utoronto.ca"
-        user.first_name = "Mehak"
-        user.last_name = "Sharma"
-        user.password = "P@assword123!"
+    def test_validate_email_invalidEmail(self):
+        self.user.email = 'invalid'
+        self.assertRaises(Exception, self.user.validate_email)
 
-        user = user.save()
-        self.assertTrue(user == None)
+    def test_save_passwordHashed(self):
+        self.user.save()
+        self.assertNotEqual(self.user.password, 'Password@123')
+
+    def test_validate_password_valid(self):
+        self.assertTrue(self.user.validate_password('Password@123'))
+
+    def test_validate_password_invalid(self):
+        self.assertFalse(self.user.validate_password('password'))
+class TestRegularUser(unittest.TestCase):
+
+    def setUp(self):
+        self.regular_user = RegularUser(
+            first_name='John',
+            last_name='Doe',
+            email='johndoe@example.com',
+            password='Password@123',
+            preferences=['preference1', 'preference2'],
+            registered_events=[]
+        )
+
+    def test_registered_events_defaultValue(self):
+        self.assertEqual(self.regular_user.registered_events, [])
+
+class TestOrganizationUser(unittest.TestCase):
+
+    def setUp(self):
+        self.organization_user = OrganizationUser(
+            name='Organization',
+            email='org@example.com',
+            password='Password@123',
+            events=[]
+        )
+
+    def test_events_defaultValue(self):
+        self.assertEqual(self.organization_user.events, [])
+
+class TestComment(unittest.TestCase):
+
+    def setUp(self):
+        self.comment = Comment(
+            author=UserInfo(author_id='123', name='John Doe'),
+            content='This is a comment',
+            rating=4,
+            replies=[]
+        )
+
+    def test_replies_defaultValue(self):
+        self.assertEqual(self.comment.replies, [])
+
+
+
+class TestEvents(unittest.TestCase):
+
+    def setUp(self):
+        self.events = Events(
+            event_date=datetime.now(),
+            location='Location',
+            title=Location(place='Place', address='Address', room='Room'),
+            targeted_preferences=['preference1', 'preference2'],
+            organizer=UserInfo(author_id='123', name='John Doe'),
+            description='Event description',
+            attendees=[UserInfo(author_id='456', name='Jane Smith')],
+            comments=[]
+        )
+
+    def test_comments_defaultValue(self):
+        self.assertEqual(self.events.comments, [])
+
+if __name__ == '__main__':
+    unittest.main()
