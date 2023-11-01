7dd640d7c1d2219c7267c9009f9fc65fee42ca55
Sebastian Czyrny <sebastian.czyrny@hotmail.com>
2023-11-01 11:50:53 -0400

    added dashboard view

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from flask import render_template, session, redirect, url_for
+from flask_login import _get_user, login_required
 from . import main
 # from .forms import NameForm
 from .. import db
@@ -11,9 +12,15 @@ Data:
     Recommended Events
     Upcoming Events
 """
-@main.route('/', methods=['GET', 'POST'])
+@main.route('/', methods=['GET'])
+@login_required
 def index():
+    user_id = _get_user()
+    user = User.objects(id=user_id).get()
 
+    recommended_events = Event.get_recommended(user.preferences)
+    upcoming_events = Event.get_upcoming(user_id)
     
     return render_template('index.html',
-                            current_time=datetime.utcnow())
+                            recommended_events=recommended_events,
+                            upcoming_events=upcoming_events)
