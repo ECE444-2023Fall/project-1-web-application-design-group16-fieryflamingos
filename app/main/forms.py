d7f6281c6f7c3ae5bb66ddd19d31eefb3f71c78a
Sebastian Czyrny <sebastian.czyrny@hotmail.com>
2023-11-02 12:48:01 -0400

    Added event form and event details routes, event form creation works

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, DateTimeField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectMultipleField
+from wtforms.fields import DateTimeLocalField
 from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
 from wtforms import ValidationError
 
-from models import Preference
+from ..models import Preference
 
 
 """ Events creation form """
@@ -12,20 +13,22 @@ class EventForm(FlaskForm):
     location_address = StringField("Location")
     location_room = StringField("Room")
 
-    title = StringField("Title", Validators=[DataRequired()])
+    title = StringField("Title", validators=[DataRequired()])
 
-    targeted_preferences = SelectField("Preferences", choices=Preference.get_preferences_as_tuple())
-    description = StringField("Description", Validators=[DataRequired(), Length(0, 1000, message="Length must be less than 1000 characters.")])
+    targeted_preferences = SelectMultipleField("Preferences", choices=Preference.get_preferences_as_tuple())
+    description = StringField("Description", validators=[DataRequired(), Length(0, 1000, message="Length must be less than 1000 characters.")])
 
-    from_date = DateTimeField("Start Date", format="'%Y-%m-%d %H:%M", validators=[DataRequired()])
-    to_date = DateTimeField("End Date", format="'%Y-%m-%d %H:%M", validators=[DataRequired()])
+    from_date = DateTimeLocalField("Start Date", format="%Y-%m-%dT%H:%M", validators=[DataRequired()])
+    to_date = DateTimeLocalField("End Date", format="%Y-%m-%dT%H:%M", validators=[DataRequired()])
 
     submit = SubmitField("Create Event")
+
 
 """ RSVPForm
 allows user to RSVP for event """
 class RSVPForm(FlaskForm):
     submit = SubmitField("RSVP")
+
 
 """ CancelRSVPForm
 allows user to cancel their RSVP for an event """
--- app/main/views.py
+++ app/main/views.py
@@ -1,24 +1,22 @@
 from datetime import datetime
-from flask import render_template, session, redirect, url_for, current_app, flash
+from flask import render_template, session, redirect, url_for, flash
 from flask_login import current_user, login_required
 from . import main
 # from .forms import NameForm
 from .. import db
-from ..models import User, Event
-from forms import EventForm
+from ..models import User, Event, Preference, Comment
+from .forms import EventForm, RSVPForm, CancelRSVPForm
 from functools import wraps
 
 
+""" org_user_required
+Decorator that checks if user is an organization """
 def org_user_required(func):
     @wraps(func)
     def decorated_view(*args, **kwargs):
         if not current_user.role == "organization":
             return redirect(url_for('errors.401'))
 
-        # flask 1.x compatibility
-        # current_app.ensure_sync is only available in Flask >= 2.0
-        if callable(getattr(current_app, "ensure_sync", None)):
-            return current_app.ensure_sync(func)(*args, **kwargs)
         return func(*args, **kwargs)
 
     return decorated_view
@@ -52,27 +50,37 @@ def base():
     return render_template('base.html')
 
 
-
 """ Event Details form
     - Allows org users to create events
+DATA: 
+    - form: EventForm
 """
-@main.route('/event/create', methods=['GET'])
+@main.route('/event/create', methods=['GET', 'POST'])
 @login_required
 @org_user_required
 def event_form():
     form = EventForm()
+    print("INSIDE HERE")
+    print("from_date: ", form.from_date.data)
+    print("to_date: ", form.to_date.data)
     if form.validate_on_submit():
+        print("place: ", form.location_place.data)
+        print("address: ", form.location_address.data)
+        print("room: ", form.location_room.data)
+        print("current_user: ", current_user.name)
+        print("title: ", form.title.data)
+        print("description: ", form.description.data)
+        print("preferences: ", form.targeted_preferences.data)
         try:
-            event = Event(location={"place": form.place.data, "location": form.location.data, "room": form.room.data},
+            event = Event(location={"place": form.location_place.data, "address": form.location_address.data, "room": form.location_room.data},
                         organizer={"author_id": current_user.id, "name": current_user.name},
                         event_date={"from_date": form.from_date.data, "to_date": form.to_date.data},
                         title=form.title.data,
                         description=form.description.data,
-                        targeted_prefernces=form.targeted_preferences.data
+                        targeted_preferences=form.targeted_preferences.data
             )
-            event.save()
-            flash('You can now login.')
-            return redirect(url_for('auth.login'))
+            event = event.save()
+            return redirect(url_for(f'main.event_details', id=str(event.id)))
         except Exception as e:
             print(e)
             pass
@@ -81,15 +89,59 @@ def event_form():
 
 """ Events detail route 
     - Takes in event id, and gives back event details
+DATA:
+    - event: Event
+    - comments: Comment[]
+    - user_is_attendee: boolean
+    - form: RSVPForm | CancelRSVPForm
+
 """
-@main.route('/event/<id>', methods=['GET'])
+@main.route('/event/<id>', methods=['GET', 'POST'])
 @login_required
-def event_details():
+def event_details(id):
+    # get event
     event = Event.get_by_id(id=id)
-    eventValid = False
-    if event:
-        eventValid = True
-    return render_template('event_details.html', event=event, eventValid=eventValid)
+
+    # Check if event is valid
+    if not event:
+        return redirect(url_for('errors.404'))
+
+    # create RSVP form
+    form = RSVPForm()
+
+    # check if user already is RSVP'ed, if so, change RSVPForm to CancelRSVPForm
+    user_is_attendee = False
+    for attendee in event.attendees:
+        if attendee.id == current_user.id:
+            user_is_attendee = True
+            form = CancelRSVPForm()
+    
+    # get preferences data, list of Preference objects
+    preferences = []
+    for pref_id in event.targeted_preferences:
+        preferences.append(Preference.get_preference_by_id(pref_id))
+    
+    # get comments for the event
+    comments = Comment.get_comments_by_event_id(id)
+
+    # VALIDATE FORMS
+    if form.validate_on_submit():
+        if isinstance(form, RSVPForm):
+            name = ""
+            if current_user.role == "regular":
+                name = f"{current_user.first_name} {current_user.last_name}"
+            else:
+                name = current_user.name
+            event = event.add_attendee(event.id, current_user.id, name)
+            user_is_attendee = True
+            form = CancelRSVPForm()
+        elif isinstance(form, CancelRSVPForm):
+            event = event.remove_attendee(event.id, current_user.id)
+            user_is_attendee = False
+            form = RSVPForm()
+
+    
+    return render_template('event_details.html', event=event, user_is_attendee=user_is_attendee, comments=comments, form=form)
 
 
 
