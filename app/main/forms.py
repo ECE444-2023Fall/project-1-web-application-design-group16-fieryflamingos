a763da6c15cc83d26037f747b5505ce92e640632
Sebastian Czyrny <sebastian.czyrny@hotmail.com>
2023-11-02 11:31:04 -0400

    Added form and views for event

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,12 +1,39 @@
-""" Registration form """
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, DateTimeField
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
+from wtforms import ValidationError
 
-
-
-""" Sign in Form """
-
+from models import Preference
 
 
 """ Events creation form """
+class EventForm(FlaskForm):
+    location_place = StringField("Place")
+    location_address = StringField("Location")
+    location_room = StringField("Room")
+
+    title = StringField("Title", Validators=[DataRequired()])
+
+    targeted_preferences = SelectField("Preferences", choices=Preference.get_preferences_as_tuple())
+    description = StringField("Description", Validators=[DataRequired(), Length(0, 1000, message="Length must be less than 1000 characters.")])
+
+    from_date = DateTimeField("Start Date", format="'%Y-%m-%d %H:%M", validators=[DataRequired()])
+    to_date = DateTimeField("End Date", format="'%Y-%m-%d %H:%M", validators=[DataRequired()])
+
+    submit = SubmitField("Create Event")
+
+""" RSVPForm
+allows user to RSVP for event """
+class RSVPForm(FlaskForm):
+    submit = SubmitField("RSVP")
+
+""" CancelRSVPForm
+allows user to cancel their RSVP for an event """
+class CancelRSVPForm(FlaskForm):
+    submit = SubmitField("Cancel RSVP")
+
+   
+
 
 
 
--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,28 @@
 from datetime import datetime
-from flask import render_template, session, redirect, url_for
+from flask import render_template, session, redirect, url_for, current_app, flash
 from flask_login import current_user, login_required
 from . import main
 # from .forms import NameForm
 from .. import db
 from ..models import User, Event
+from forms import EventForm
+from functools import wraps
+
+
+def org_user_required(func):
+    @wraps(func)
+    def decorated_view(*args, **kwargs):
+        if not current_user.role == "organization":
+            return redirect(url_for('errors.401'))
+
+        # flask 1.x compatibility
+        # current_app.ensure_sync is only available in Flask >= 2.0
+        if callable(getattr(current_app, "ensure_sync", None)):
+            return current_app.ensure_sync(func)(*args, **kwargs)
+        return func(*args, **kwargs)
+
+    return decorated_view
+
 
 
 """ Dashboard:
@@ -15,7 +33,6 @@ Data:
 @main.route('/', methods=['GET'])
 @login_required
 def index():
-
     user = current_user
     try:
         recommended_events = Event.get_recommended(user.preferences)
@@ -27,22 +44,52 @@ def index():
     except Exception as e:
         return render_template('index.html')
 
-    print("in index route...")
-    return render_template('index.html',
-                            current_time=datetime.utcnow())
+ 
 
 """ Basic Nav Bar Template"""
 @main.route('/base', methods=['GET'])
 def base():
     return render_template('base.html')
 
-""" Event Details form"""
-@main.route('/event', methods=['GET'])
-def events():
-    return render_template('events.html')
 
 
-""" Events detail route """
+""" Event Details form
+    - Allows org users to create events
+"""
+@main.route('/event/create', methods=['GET'])
+@login_required
+@org_user_required
+def event_form():
+    form = EventForm()
+    if form.validate_on_submit():
+        try:
+            event = Event(location={"place": form.place.data, "location": form.location.data, "room": form.room.data},
+                        organizer={"author_id": current_user.id, "name": current_user.name},
+                        event_date={"from_date": form.from_date.data, "to_date": form.to_date.data},
+                        title=form.title.data,
+                        description=form.description.data,
+                        targeted_prefernces=form.targeted_preferences.data
+            )
+            event.save()
+            flash('You can now login.')
+            return redirect(url_for('auth.login'))
+        except Exception as e:
+            print(e)
+            pass
+    return render_template('event_create.html', form=form)
+
+
+""" Events detail route 
+    - Takes in event id, and gives back event details
+"""
+@main.route('/event/<id>', methods=['GET'])
+@login_required
+def event_details():
+    event = Event.get_by_id(id=id)
+    eventValid = False
+    if event:
+        eventValid = True
+    return render_template('event_details.html', event=event, eventValid=eventValid)
 
 
 
