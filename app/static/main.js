9f09800eed186dd932b78c4ca7c83ff785a0ba43
Mohammed <muji-amir@hotmail.com>
2023-11-10 22:57:16 -0500

    basic version of calendar

--- app/static/main.js
+++ app/static/main.js
@@ -1,7 +1,8 @@
-const dayscontainer = document.querySelector(".days"),
+const daysContainer = document.querySelector(".days"),
     nextBtn = document.querySelector(".next-btn"),
     prevBtn = document.querySelector(".prev-btn"),
     month = document.getElementById("month");
+    todayBtn = document.querySelector(".today-btn");
 
 const months = [
     "January",
@@ -26,7 +27,7 @@ let currentMonth = date.getMonth();
 let currentYear = date.getFullYear();
 
 function renderCalendar() {
-    date.getDate(1);
+    date.setDate(1);
     const firstDay = new Date(currentYear, currentMonth, 1);
     const lastDay = new Date(currentYear, currentMonth + 1, 0);
     const lastDayIndex = lastDay.getDay();
@@ -36,6 +37,66 @@ function renderCalendar() {
     const nextDays = 7 - lastDayIndex - 1;
 
     month.innerHTML = `${months[currentMonth]} ${currentYear}`;
+
+    let days = "";
+    
+    for(let x = firstDay.getDay(); x > 0; x++){
+        days = `<div class="days prev"> ${prevLastDayDate - x + 1}</div>`;
+    }
+
+    for(let i = 1; i <= lastDayDate; i++){
+        if(
+            i === new Date.getDate() && 
+            currentMonth === new Date().getMonth() &&
+            currentYear === new Date().getFullYear()
+        ){
+            days += `<div class="day today">${i}</div>`;
+        }else{
+            days += `<div class="day">${i}</div>`;
+        }
+    }
+
+    for(let j = 1; j <= nextDays; j++){
+        days += `<div class="day next">${j}</div>`;
+    }
+
+    hideTodayBtn();
+    daysContainer.innerHTML = days;
 }
 
 renderCalendar();
+
+nextBtn.addEventListener("click", () => {
+    currentMonth++;
+    if(currentMonth > 11){
+        currentMonth = 0;
+        currentYear++;
+    }
+    renderCalendar();
+});
+
+prevBtn.addEventListener("click", () => {
+    currentMonth--;
+    if(currentMonth < 0){
+        currentMonth = 11;
+        currentYear--;
+    }
+    renderCalendar();
+});
+
+todayBtn.addEventListener("click", () => {
+    currentMonth = date.getMonth();
+    currentYear = date.getFullYear;
+    renderCalendar();
+});
+
+function hideTodayBtn(){
+    if(
+        currentMonth === new Date().getMonth() &&
+        currentYear === new Date().getFullYear()
+    ){
+        todayBtn.style.display = "none";
+    }else {
+        todayBtn.style.display = "flex";
+    }
+}
