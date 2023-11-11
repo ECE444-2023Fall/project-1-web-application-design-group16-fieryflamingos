5ea6802a7d26fe9bccfef1ab5375b5b95518dc79
Sebastian Czyrny <sebastian.czyrny@hotmail.com>
2023-11-11 14:42:38 -0500

    Production configuration set

--- occasional.py
+++ occasional.py
@@ -2,7 +2,7 @@ import os
 from app import create_app, db
 # from app.models import User
 
-app = create_app('default')
+app = create_app('production')
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db)
