a087f274cdd4b0babcae0cc6d9a05be49ed4c0a1
DYLAN SUN <zhangjie.sun@mail.utoronto.ca>
2023-11-13 20:23:31 -0500

    comment based integration tests

--- tests/integration_tests.py
+++ tests/integration_tests.py
@@ -68,7 +68,70 @@ class TestIntegration(unittest.TestCase):
         fetched_event = Event.objects(title='Integration Test Event').get()
         self.assertIn(comment.id, fetched_event.comments)
 
+    def test_organization_user_and_event_interaction(self):
+        # Create an organization user
+        org_user = OrganizationUser(
+            email='johndoe_org@mail.utoronto.ca',
+            password='Password@123',
+            name='Test Organization'
+        )
+        org_user.save()
+
+        # Create an event organized by the organization user
+        fixed_time = datetime(2023, 10, 23, 12, 0)
+        event = Event(
+            event_date={"from_date": fixed_time, "to_date": fixed_time},
+            title='Integration Test Event',
+            location={"place": 'Place', "address": 'Address', "room": 'Room'},
+            targeted_preferences=['preference1', 'preference2'],
+            organizer={"author_id": org_user.id, "name": org_user.name},
+            description='Event description',
+            attendees=[]
+        )
+        event.save()
+
+        # Fetch the user and check if the event is associated with the organization user
+        fetched_org_user = OrganizationUser.objects(email=org_user.email).get()
+        self.assertIn(event.id, fetched_org_user.organized_events)
     
+    def test_event_and_reply_interaction(self):
+        # Create an event
+        fixed_time = datetime(2023, 10, 23, 12, 0)
+        event = Event(
+            event_date={"from_date": fixed_time, "to_date": fixed_time},
+            title='Integration Test Event',
+            location={"place": 'Place', "address": 'Address', "room": 'Room'},
+            targeted_preferences=['preference1', 'preference2'],
+            organizer={"author_id": 1, "name": "Test Organizer"},
+            description='Event description',
+            attendees=[]
+        )
+        event.save()
+
+        # Add a comment to the event
+        comment = EventComment(
+            event_id=event.id,
+            author={"author_id": 1, "name": "Test User"},
+            content="Integration Test Comment",
+            likes=0,
+            rating=5
+        )
+        comment.save()
+
+        # Add a reply to the comment
+        reply = Reply(
+            event_id=event.id,
+            author={"author_id": 1, "name": "Test User"},
+            content="Integration Test Reply",
+            likes=0,
+            reply_to_id=comment.id
+        )
+        reply.save()
+
+        # Fetch the event and check if the reply is associated with the comment
+        fetched_event = Event.objects(title='Integration Test Event').get()
+        fetched_comment = EventComment.objects(content='Integration Test Comment').get()
+        self.assertIn(reply.id, fetched_comment.replies)
 
 if __name__ == '__main__':
     unittest.main()
